80386af3c35c3e4dfbccf70115dc4c0565552d1d
Lukasz Lacinski <lukasz@uchicago.edu>
2019-04-16 13:27:24 -0500

    Update transfer.py

--- src/tools/fileTransferTool/fileTransfer.py
+++ src/tools/fileTransferTool/fileTransfer.py
@@ -1,30 +1,75 @@
 import sys
+import json
 import globus_sdk
 
-pickled_tokens = base64.b64decode(sys.argv[0])
+output = sys.argv[1]
+print(output)
 
-tokens = pickle.loads(pickled_tokens)
-isinstance(tokens, dict)
+tokens = None
+with open("/srv/galaxy/server/database/jobs_directory/.dev-tokens.json") as tokens_file:
+    tokens = json.load(tokens_file)
 
-transfer_access_token = tokens['tokens']['transfer.api.globus.org']['access_token']
+transfer_tokens = tokens["transfer.api.globus.org"]
+transfer_access_token = transfer_tokens["access_token"]
 transfer_authorizer = globus_sdk.AccessTokenAuthorizer(transfer_access_token)
 
 tc = globus_sdk.TransferClient(authorizer=transfer_authorizer)
 
-source_endpoint_id = "ebf55996-33bf-11e9-9fa4-0a06afd4a22e"
-source_path = "/personal/rick/galaxy-training-data/"
+# petrel#ncipilot
+src_ep = "ebf55996-33bf-11e9-9fa4-0a06afd4a22e"
+src_path = "/personal/rick/galaxy-training-data/mutant_R1.fastq"
 
-dest_endpoint_id = "bfe3af54-5fcc-11e9-bf34-0edbf3a4e7ee"
-dest_path = "/"
+# fairdata#dev1share (Galaxy instance endpoint)
+dst_ep = "bfe3af54-5fcc-11e9-bf34-0edbf3a4e7ee"
+dst_path = output
 
 label = "My tutorial transfer"
 
 # TransferData() automatically gets a submission_id for once-and-only-once submission
-tdata = globus_sdk.TransferData(tc, source_endpoint_id, dest_endpoint_id, label=label)
-tdata.add_item(source_path, dest_path, recursive=True)
+tdata = globus_sdk.TransferData(tc, src_ep, dst_ep, label=label)
+tdata.add_item(src_path, dst_path, recursive=False)
 
-tc.endpoint_autoactivate(source_endpoint_id)
-tc.endpoint_autoactivate(dest_endpoint_id)
+tc.endpoint_autoactivate(src_ep)
+tc.endpoint_autoactivate(dst_ep)
 
-submit_result = tc.submit_transfer(tdata)
+try:
+    task = tc.submit_transfer(tdata)
+except Exception as e:
+    sys.stderr.write("Globus transfer from {}{} to {}{} failed due to error: {}".format(
+            src_ep, src_path, dst_ep, dst_path, e))
+    sys.exit(1)
+
+last_event_time = None
+"""
+A Globus transfer job (task) can be in one of the three states: ACTIVE, SUCCEEDED, FAILED.
+The tool every 20 seconds polls a status of the transfer job (task) from the Globus Transfer service,
+with 60 second timeout limit. If the task is ACTIVE after time runs out 'task_wait' returns False,
+and True otherwise.
+"""
+while not tc.task_wait(task["task_id"], 60, 15):
+    task = tc.get_task(task["task_id"])
+    # Get the last error Globus event
+    events = tc.task_event_list(task["task_id"], num_results=1, filter="is_error:1")
+    event = events.data[0]
+    # Print the error event if it was not yet printed
+    if event["time"] != last_event_time:
+        last_event_time = event["time"]
+        # TODO: printing the error event
+
+"""
+The Globus transfer job (task) has been terminated (is not ACTIVE). Check if the transfer
+SUCCEEDED or FAILED.
+"""
+task = tc.get_task(task["task_id"])
+if task["status"] == "SUCCEEDED":
+    # "Globus transfer {}, from {}{} to {}{} succeeded".format(
+    #    task["task_id"], src_ep, src_path, dst_ep, dst_path))
+    sys.exit(0)
+else:
+    sys.stderr.write("Globus Transfer task: {}\n".format(task))
+    events = tc.task_event_list(task["task_id"], num_results=1, filter="is_error:1")
+    event = events.data[0]
+    sys.stderr.write("Globus transfer {}, from {}{} to {}{} failed due to error: '{}'".format(
+        task["task_id"], src_ep, src_path, dst_ep, dst_path, event["details"]))
+    sys.exit(1)
 print("Task ID:", submit_result["task_id"])
